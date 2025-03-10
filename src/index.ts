63a8fba2aaadf99c39294b9b750ac9aee30937c7
Shinya Fujino <shf0811@gmail.com>
2025-03-11 01:53:55 +0900

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,78 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { parseHTML } from 'linkedom';
+import OpenAI from 'openai';
+import { Resend } from 'resend';
+
+type Bindings = {
+	EMAIL_FROM: string;
+	EMAIL_TO: string;
+	OPENAI_API_KEY: string;
+	RESEND_API_KEY: string;
+};
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const topStories = (await fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+			.then((response) => response.json() as Promise<number[]>)
+			.then((ids) => ids.slice(0, 50))
+			.then((ids) =>
+				Promise.all(ids.map((id) => fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then((response) => response.json()))),
+			)
+			.then((stories) => stories.sort((a: any, b: any) => b.score - a.score).slice(0, 3))) as {
+			id: number;
+			title: string;
+			url: string;
+			kids: number[];
+		}[];
+
+		const news = await Promise.all(
+			topStories.map(async (story) => {
+				const newsDom = parseHTML(await fetch(story.url).then((response) => response.text()));
+				const comments = await Promise.all(
+					story.kids.map(async (id) => {
+						const comment = (await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then((response) => response.json())) as {
+							text: string;
+						};
+						return comment.text;
+					}),
+				);
+				return {
+					title: story.title,
+					url: story.url,
+					news: newsDom.window.document.querySelector('body')?.textContent as string,
+					comments,
+				};
+			}),
+		);
+
+		const client = new OpenAI({
+			apiKey: env.OPENAI_API_KEY,
+		});
+		const completion = await client.chat.completions.create({
+			model: 'gpt-4o-mini',
+			messages: [
+				{
+					role: 'user',
+					content: `The following is data collected from 3 top news articles and comments from Hacker News.
+Please summarize the content of the articles and describe how people are reacting to them in the comments.
+
+Please output in the following format:
+- Title: <title> (URL: <url>)
+- Summary: <summary>
+- How people are reacting: <comments>
+
+${JSON.stringify(news)}`,
+				},
+			],
+		});
+
+		const resend = new Resend(env.RESEND_API_KEY);
+		await resend.emails.send({
+			from: env.EMAIL_FROM,
+			to: env.EMAIL_TO,
+			subject: "Today's Hacker News",
+			text: completion.choices[0].message.content ?? '',
+		});
+
+		return new Response('ok');
 	},
-} satisfies ExportedHandler<Env>;
+} satisfies ExportedHandler<Bindings>;
